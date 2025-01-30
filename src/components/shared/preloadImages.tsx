637905bee7f5e8042cdb6c34ce4365017ee91e1e
aborile <37267486+aborile@users.noreply.github.com>
2025-01-31 00:10:54 +0900

    fix: preload 이미지 수정

--- src/components/shared/preloadImages.tsx
+++ src/components/shared/preloadImages.tsx
@@ -4,13 +4,12 @@ import FingerspellImage from "./fingerspellImage";
 
 function PreloadImages() {
   return (
-    <div className="invisible">
+    <div className="fixed h-10 invisible overflow-hidden top-0 -z-10">
       {Array.from({ length: 14 }).map((_, index) => (
         <FingerspellImage
           key={index}
           type="consonant"
           sign={index.toString().padStart(2, "0")}
-          size={1}
         />
       ))}
       {Array.from({ length: 17 }).map((_, index) => (
@@ -18,7 +17,6 @@ function PreloadImages() {
           key={index}
           type="vowel"
           sign={index.toString().padStart(2, "0")}
-          size={1}
         />
       ))}
     </div>
