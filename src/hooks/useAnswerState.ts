e8f382e7339652d2ff89d213c5b59e7bc31f3aeb
aborile <37267486+aborile@users.noreply.github.com>
2025-01-31 18:01:49 +0900

    fix: 다음 단어를 가져오는 로딩 시간을 제거

--- src/hooks/useAnswerState.ts
+++ src/hooks/useAnswerState.ts
@@ -2,15 +2,19 @@
 
 import { getNewWord } from "@/services/word";
 import { PlayingState } from "@/typings";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export function useAnswerState() {
   const [answer, setAnswer] = useState<string>("");
+  // 다음 정답 단어를 저장하는 ref -> 다음 단어를 가져오는 로딩 시간을 제거
+  const nextAnswer = useRef("");
+
   const [playingState, setPlayingState] = useState<PlayingState>("idle");
 
   // 최초 정답 단어를 가져옴
   useEffect(() => {
     getNewWord().then(setAnswer);
+    getNewWord().then((word) => (nextAnswer.current = word));
   }, []);
 
   const restartAnimation = useCallback(() => {
@@ -20,10 +24,21 @@ export function useAnswerState() {
     setPlayingState("idle");
   }, []);
   const handleNext = useCallback(async () => {
-    setAnswer("");
-    const newAnswer = await getNewWord();
-    setAnswer(newAnswer);
-    setPlayingState("idle");
+    // 다음 정답 단어가 없으면 새로운 단어를 가져옴
+    if (!nextAnswer.current) {
+      setAnswer("");
+      const newAnswer = await getNewWord();
+      setAnswer(newAnswer);
+      setPlayingState("idle");
+      const newWord = await getNewWord();
+      nextAnswer.current = newWord;
+    } else {
+      setAnswer(nextAnswer.current);
+      setPlayingState("idle");
+      nextAnswer.current = "";
+      const newWord = await getNewWord();
+      nextAnswer.current = newWord;
+    }
   }, []);
 
   const changeAnswer = useCallback((newAnswer: string) => {
