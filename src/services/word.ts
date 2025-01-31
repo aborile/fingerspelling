9168fccdd5ad61aeda2b64b76009e43d0755c471
aborile <37267486+aborile@users.noreply.github.com>
2025-01-31 18:01:37 +0900

    refactor: 오픈사전 api 조회 로직을 next 서버로 분리

--- src/services/word.ts
+++ src/services/word.ts
@@ -1,42 +1,11 @@
 import axios from "axios";
 
-import { configs, SEARCH_KEY, WORDS } from "@/constants";
-import { OpendictResult } from "@/typings/opendict";
+import { WORDS } from "@/constants";
 
 export async function getNewWord() {
   try {
-    // 검색할 키워드를 SEARCH_KEY 배열에서 랜덤 선택
-    const q = SEARCH_KEY[Math.floor(Math.random() * SEARCH_KEY.length)];
-
-    // 오픈사전 API를 통해 해당 키워드에 대한 검색 결과를 가져옴
-    const { data } = await axios.get<OpendictResult>(
-      "https://opendict.korean.go.kr/api/search",
-      {
-        params: {
-          key: configs.OPENDICT_API_KEY,
-          q,
-          req_type: "json",
-          num: 50,
-          sort: "popular",
-          advanced: "y",
-          method: Math.random() > 0.5 ? "start" : "include",
-          type1: "word",
-          type3: "general",
-        },
-      }
-    );
-
-    // 검색 결과에서 단어만 추출, 한글이 아닌 문자는 제거, 길이가 2~4인 단어만 필터링
-    const results = data.channel.item
-      .map(({ word }) => word.replace(/[^가-힣]/g, ""))
-      .filter((word) => word.length > 1 && word.length < 5);
-
-    // 검색 결과가 없을 경우 에러
-    if (results.length === 0) throw new Error("No results");
-
-    // 검색 결과 중 랜덤 선택
-    const randomIndex = Math.floor(Math.random() * results.length);
-    return results[randomIndex];
+    const { data } = await axios.get<{ word: string }>("/api/word");
+    return data.word;
   } catch {
     // 검색 결과가 없거나, 검색에 실패한 경우 WORDS 배열에서 랜덤 선택
     const randomIndex = Math.floor(Math.random() * WORDS.length);
